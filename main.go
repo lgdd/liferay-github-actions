26aa3fbec01b565a00d127de7272f7f9971de52a
lgdd <louis-guillaume.durand@liferay.com>
2023-06-04 17:49:03 +0200

    chore: add walk function

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+)
 
 func main() {
 	fmt.Println("hello world")
+	filepath.Walk("./cloud-repo", func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() && info.Name() == "Dockerfile" {
+			fmt.Println(fmt.Sprintf("Found Dockerfile under %s", path))
+		}
+		return nil
+	})
 }
